b04ddd778947dcd62006c5e589e35236e42296c9
wojteknwk <wojteknwk@o2.pl>
2016-02-09 21:31:52 +0100

    1 17

--- app/src/main/java/com/example/wnwk/calcsple/MainActivity.java
+++ app/src/main/java/com/example/wnwk/calcsple/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.wnwk.calcsple;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      /*  if (BuildConfig.DEBUG) {
+            Log.d(Constants.LOG, "onCreated called");
+        }*/
         setContentView(R.layout.activity_main);
     }
+
+
+    public void onClick (View view) {
+        EditText input = (EditText) findViewById(R.id.main_input);
+        String string = input.getText().toString();
+        Toast.makeText(this, string,
+                Toast.LENGTH_LONG).show();
+    }
+
 }
